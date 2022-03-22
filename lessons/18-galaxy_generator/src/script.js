2e0c1a59e456fa9125520229b1f5af9ca5127ec0
Anders Olesen <anders242@gmail.com>
2022-03-22 18:28:05 +0000

    add branch angles

--- lessons/18-galaxy_generator/src/script.js
+++ lessons/18-galaxy_generator/src/script.js
@@ -7,7 +7,8 @@ import * as dat from 'lil-gui'
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI({ width: 380})
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -22,28 +23,42 @@ const scene = new THREE.Scene()
 // Geometry
 
 const parameters = {}
-parameters.count = 1000
-parameters.size = 0.02
+parameters.count = 100000
+parameters.size = 0.01
+parameters.radius = 5
+parameters.branches = 3
+
+let geometry = null
+let material = null
+let points = null
 
 
 const galaxyGenerator = () => 
 {
-    const geometry = new THREE.BufferGeometry()
+    if(points !== null) 
+    {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+    geometry = new THREE.BufferGeometry()
 
     const positions = new Float32Array(parameters.count * 3)
 
     for(let i = 0 ; i < parameters.count ; i ++) 
     {
         const i3 = i * 3
-        positions[i3 + 0] = (Math.random() - 0.5) * 3
-        positions[i3 + 1] = (Math.random() - 0.5) * 3
-        positions[i3 + 2] = (Math.random() - 0.5) * 3
+
+        const radius = Math.random() * parameters.radius
+        positions[i3 + 0] = radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = 0
     }
 
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
 
     // Material 
-const material = new THREE.PointsMaterial({
+material = new THREE.PointsMaterial({
     size : parameters.size,
     sizeAttenuation : true,
     depthWrite: false,
@@ -52,14 +67,17 @@ const material = new THREE.PointsMaterial({
 
 //points 
 
-const points = new THREE.Points(geometry, material)
+points = new THREE.Points(geometry, material)
 scene.add(points)
 }
 
 
 galaxyGenerator()
-
-
+//he always uses min as the step as well
+gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(galaxyGenerator)
+gui.add(parameters, 'size').min(0.01).max(0.1).step(0.01).onFinishChange(galaxyGenerator)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(galaxyGenerator)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(galaxyGenerator)
 
 
 
