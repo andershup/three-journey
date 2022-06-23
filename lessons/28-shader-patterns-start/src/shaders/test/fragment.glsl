7fcfcfc8f4baa2b188d1e1767b0c0bbf7b2519ed
Anders Olesen <anders242@gmail.com>
2022-06-23 16:03:29 +0100

    add multiple practice glsl shader patterns

--- lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
+++ lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
@@ -2,5 +2,12 @@ varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(vUv.x, vUv.y, 1.0, 1.0);
+    // pattern 3
+    // float barX = step(0.4 , mod(vUv.x * 10.0-0.2, 1.0)) * step(0.8 , mod(vUv.y * 10.0 , 1.0));
+    // float barY = step(0.8 , mod(vUv.x * 10.0 , 1.0)) * step(0.4 , mod(vUv.y * 10.0-0.2, 1.0));
+    // float strength = barX + barY;
+   
+    float strength = step(0.2, max(abs(vUv.y - 0.5), abs(vUv.x - 0.5)));
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    
 }
