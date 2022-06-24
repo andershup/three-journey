20cbb34e97111b443898207dc481f033f0163a6d
Anders Olesen <anders242@gmail.com>
2022-06-24 13:05:10 +0100

    add new glsl files

--- lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
+++ lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
@@ -2,12 +2,8 @@ varying vec2 vUv;
 
 void main()
 {
-    // pattern 3
-    // float barX = step(0.4 , mod(vUv.x * 10.0-0.2, 1.0)) * step(0.8 , mod(vUv.y * 10.0 , 1.0));
-    // float barY = step(0.8 , mod(vUv.x * 10.0 , 1.0)) * step(0.4 , mod(vUv.y * 10.0-0.2, 1.0));
-    // float strength = barX + barY;
-   
-    float strength = step(0.2, max(abs(vUv.y - 0.5), abs(vUv.x - 0.5)));
-    gl_FragColor = vec4(strength, strength, strength, 1.0);
+
+    // float strength = vUv.x;
+    gl_FragColor = vec4(vUv.x, vUv.x,  vUv.x, 1.0);
     
 }
