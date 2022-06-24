28c562d2e0ea14efe871b7812d9ec10c25b42fc9
Anders Olesen <anders242@gmail.com>
2022-06-24 15:27:53 +0100

    add particle patterns

--- lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
+++ lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
@@ -2,8 +2,13 @@ varying vec2 vUv;
 
 void main()
 {
-
-    // float strength = vUv.x;
-    gl_FragColor = vec4(vUv.x, vUv.x,  vUv.x, 1.0);
+    // float xBar = step(0.4, mod(vUv.x * 10.0-0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float yBar = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4 , mod(vUv.y * 10.0-0.2, 1.0));
+    // float yBar = step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float strength = xBar + yBar;
+    // float strength = step(0.4, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    float strength = floor(vUv.x * 10.0) /10.0;
+    strength *= floor(vUv.y * 10.0) /10.0;
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
     
 }
