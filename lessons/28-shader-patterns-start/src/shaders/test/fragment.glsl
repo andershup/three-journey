fa268c725288438133e83b9cb0f62ad45082fb5f
Anders Olesen <anders242@gmail.com>
2022-06-26 12:41:50 +0100

    add random grid shaders

--- lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
+++ lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
@@ -1,5 +1,11 @@
 varying vec2 vUv;
 
+float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+
 void main()
 {
     // float xBar = step(0.4, mod(vUv.x * 10.0-0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
@@ -7,8 +13,20 @@ void main()
     // float yBar = step(0.8, mod(vUv.y * 10.0, 1.0));
     // float strength = xBar + yBar;
     // float strength = step(0.4, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
-    float strength = floor(vUv.x * 10.0) /10.0;
-    strength *= floor(vUv.y * 10.0) /10.0;
-    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    // float strength = floor(vUv.x * 10.0) /10.0;
+// float strength = step(0.8,(mod(vUv.x * 10.0, 1.0))) *  step(0.8,(mod(vUv.y * 10.0, 1.0)));
+// float strength = step(0.3,(mod(vUv.x * 10.0, 1.0))) *  step(0.8,(mod(vUv.y * 10.0, 1.0)));
+
+// float barX = step(0.3,(mod(vUv.x * 10.0 - 0.2, 1.0))) *  step(0.8,(mod(vUv.y * 10.0, 1.0)));
+// float barY = step(0.3,(mod(vUv.y * 10.0, 1.0))) *  step(0.8,(mod(vUv.x * 10.0 - 0.2, 1.0)));
+// float strength = barX + barY; 
+
+// float strength =  max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+// strength = step(0.4, strength);
+
+vec2 gridUv = vec2((floor(vUv.x * 10.0) / 10.0) * (floor(vUv.y * 10.0) / 10.0));
+
+ float strength = random(gridUv);
+    gl_FragColor = vec4(strength,strength, strength, 1.0);
     
 }
