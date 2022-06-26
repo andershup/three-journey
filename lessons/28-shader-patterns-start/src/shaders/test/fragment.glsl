320aecaaafe8faa43e5e4fe0db5a82de8235b32e
Anders Olesen <anders242@gmail.com>
2022-06-26 16:15:24 +0100

    add shades up to number 36

--- lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
+++ lessons/28-shader-patterns-start/src/shaders/test/fragment.glsl
@@ -1,8 +1,17 @@
+#define PI 3.1415926535897932384626433832795
 varying vec2 vUv;
 
 float random(vec2 st)
 {
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
 }
 
 
@@ -24,9 +33,19 @@ void main()
 // float strength =  max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
 // strength = step(0.4, strength);
 
-vec2 gridUv = vec2((floor(vUv.x * 10.0) / 10.0) * (floor(vUv.y * 10.0) / 10.0));
+// vec2 gridUv = vec2((floor(vUv.x * 10.0) / 10.0) * (floor(vUv.y * 10.0) / 10.0));
 
- float strength = random(gridUv);
+//  float strength = random(gridUv);
+// float strength =  0.015 / distance(vUv, vec2(0.5, 0.5));
+
+//    float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+//     strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+//     float rotating = rotate(vUv, PI * 0,25, vec2(0.5));
+    
+        // // Pattern 19
+    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5))-0.25));
+       // // Pattern 27
+    // float strength = distance(vUv, vec2(0.5));
     gl_FragColor = vec4(strength,strength, strength, 1.0);
     
 }
